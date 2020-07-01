f6399d2fcf8bd9b57e813e6b5ad8b15d124cd418
paul <727736071@qq.com>
2020-07-01 11:29:25 +0800

    master2.0.1

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,6 @@ package test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test.main master");
         System.out.println("Test.main 2.0.1");
     }
 }
