56182a94267d1ba9e47e7f14de7da2c779f06f68
paul <727736071@qq.com>
2020-07-01 09:48:24 +0800

    test1.0

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,6 @@ package test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test.main");
+        System.out.println("Test.main 1.0");
     }
 }
