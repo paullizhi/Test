aef45001a029a7b5e1ea8a249a3088adea6c2bf7
paul <727736071@qq.com>
2020-07-01 10:29:52 +0800

    master.

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,6 @@ package test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test.main master");
-        System.out.println("Test.main 1.0.1");
+        System.out.println("Test.main master.");
     }
 }
