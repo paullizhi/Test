a6b4f77b119d7aed57fe261f7dae903916037658
paul <727736071@qq.com>
2020-07-01 09:37:42 +0800

    test

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Administrator on 2020\6\23 0023.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+    }
+}
